d49ddc3e1c597de7a66ef7ec62ba390f4d959f45
rajavendan <122148087+Rajavendan@users.noreply.github.com>
2026-05-12 08:49:36 +0530

    Update firebase.ts

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAf2ZMpwOoRi3cuBOeFLPB9X97IN4iByCE",
+  apiKey: "AIzaSydjff vkfvfvfj vfuvbvf vk9X97IN4iByCE///temp fake api key",
   authDomain: "currency-converter-60305.firebaseapp.com",
   projectId: "currency-converter-60305",
   storageBucket: "currency-converter-60305.firebasestorage.app",
